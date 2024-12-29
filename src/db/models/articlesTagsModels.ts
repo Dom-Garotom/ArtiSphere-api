fbdc7391cf35f78189d258e678c3c5392433eb9a
Gean ferrera <145035724+Dom-Garotom@users.noreply.github.com>
2024-12-29 15:15:06 -0300

    feat: create a intermediate table called articles tags

---
+++ src/db/models/articlesTagsModels.ts
@@ -0,0 +1,38 @@
+import { dataBase } from './../config/config';
+import { DataTypes, Model, UUIDV4 } from "sequelize";
+
+interface ArticleTagsDBAtributes {
+    id: string;
+    article_id: string;
+    tags_id: string;
+}
+
+class ArticleTagsDB extends Model<ArticleTagsDBAtributes> implements ArticleTagsDBAtributes {
+    public readonly id!: string;
+    public readonly article_id!: string;
+    public readonly tags_id!: string;
+    public readonly createAt?: Date;
+    public readonly updateAt?: Date;
+}
+
+ArticleTagsDB.init({
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: UUIDV4
+    },
+
+    article_id: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+
+    tags_id: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+}, {
+    sequelize: dataBase,
+    tableName: "articles_tags"
+})
